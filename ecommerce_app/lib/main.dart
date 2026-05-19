7a381be2bb089a9f9f71772eed8ab77c179e882e
PeterBeshoy <peterpeshoy8@gmail.com>
2026-05-19 14:11:39 +0300

    localization and services

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:ecommerce_app/core/constant/color.dart';
+import 'package:ecommerce_app/core/localization/translation.dart';
+import 'package:ecommerce_app/core/services/services.dart';
 import 'package:ecommerce_app/routes.dart';
 import 'package:ecommerce_app/view/screen/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialServices();
   runApp(const MyApp());
 }
 
@@ -14,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      translations: MyTranslation(),
       debugShowCheckedModeBanner: false,
       title: 'flutter demo',
       theme: ThemeData(
