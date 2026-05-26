d4c65a738ad049f8ec218f40947ed98408b2b825
PeterBeshoy <peterpeshoy8@gmail.com>
2026-05-26 19:28:40 +0300

    Login page 1

--- ecommerce_app/lib/view/screen/auth/login.dart
+++ ecommerce_app/lib/view/screen/auth/login.dart
@@ -7,10 +7,48 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login"),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text("Sign In", style: TextStyle(color: Colors.black)),
       ),
-      body: const Center(
-        child: Text("Login Screen"),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        child: ListView(
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              "Welcome Back!",
+              style: Theme.of(
+                context,
+              ).textTheme.headlineLarge?.copyWith(fontSize: 35),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              child: Text(
+                "Sign In with your email and password or continue with social media",
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 40),
+            TextFormField(
+              decoration: const InputDecoration(
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                contentPadding: EdgeInsets.symmetric(vertical: 5 , horizontal: 20),
+                label: Text("Email"),
+                hintText: "Enter your email",
+                suffixIcon: Icon(Icons.email_outlined),
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
